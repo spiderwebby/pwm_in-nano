b152154cc0ecfd18b58281c889fe05d06a3e1505
spiderwebby <spiderwebby@gmail.com>
2025-01-07 08:24:12 +0000

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@ const int inPin1 = 7;
 const int inPin2 = 8;
 const int inPin3 = 9;
 
-
+byte prev_PINB;
+byte prev_PIND;
 
 void setup() {
   PCICR  |= B00000101; //pinchange inturupt port B & D (pins 0-13)
@@ -17,9 +18,28 @@ void loop() {
   // put your main code here, to run repeatedly:
 }
 
-ISR (PCINT0_vect)
+ISR (PCINT0_vect) //port B isr
 {
   
+  switch(PINB)    //PINB is the pin values of port B
+  case B00000000:
+    break;
+  case B00000001:
+
+    break;
+  case B00000010:
+
+    break;
+  case B00000100:
+
+    break;
+  case B00001000:
+
+    break;
+  case B00010000:
+
+    break;
+
 }
 
 ISR (PCINT2_vect)
