422b60af15c97509b4b37a06c9989a4c4638b784
spiderwebby <spiderwebby@gmail.com>
2025-01-07 16:49:53 +0000

    ISRs and print loop sorted. untested.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 const int inPin1 = 7;
 const int inPin2 = 8;
 const int inPin3 = 9;
+volatile bool prev_pinvalue[15];
+volatile unsigned int pinmicros[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+volatile unsigned int pin_high_time[15];
 
 byte prev_PINB;
 byte prev_PIND;
@@ -12,38 +15,69 @@ void setup() {
   PCICR  |= B00000101; //pinchange inturupt port B & D (pins 0-13)
   PCMSK0 |= B00011111; //D12-D8 port B
   PCMSK2 |= B11111100; // D7-D2 port D
+
+  Serial.begin(115200);
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-ISR (PCINT0_vect) //port B isr
+void loop() 
 {
-  
-  switch(PINB)    //PINB is the pin values of port B
-  case B00000000:
-    break;
-  case B00000001:
-
-    break;
-  case B00000010:
-
-    break;
-  case B00000100:
-
-    break;
-  case B00001000:
-
-    break;
-  case B00010000:
-
-    break;
-
+  Serial.print("$srvo,");
+  //we're not actually using all the pins, so we skip a bunch at the start and in the middle
+  for(int i = 2; i<8;i++)  
+  {
+    Serial.print(pinmicros[i]);
+    Serial.print(",");
+  }
+  for(int i = 8; i<12;i++)
+  {
+    Serial.print(pinmicros[i]);
+    Serial.print(",");
+  }
+  Serial.println(pinmicros[12]);
 }
 
-ISR (PCINT2_vect)
+//these are defined *elsewhere ooooOOOOooo*
+//we have to work out which pin changed, and if it's falling or rising
+ISR (PCINT0_vect) //port B isr //D12-D8 port B
 {
-  
+  unsigned int isrMicros = micros();
+  for(int i = 8; i<13;i++)
+  {
+    if(digitalRead(i) != prev_pinvalue[i])
+    {
+      if(prev_pinvalue[i]==0) //rising edge
+      {
+        prev_pinvalue[i] = 1;
+        pinmicros[i] = isrMicros;
+      }
+      else //falling edge
+      {
+        prev_pinvalue[i] = 0;
+        pinmicros[i] = isrMicros - pinmicros[i];
+      }
+    }
+  }
+}
+
+ISR (PCINT2_vect)// D7-D2 port D
+{
+  unsigned int isrMicros = micros();
+  for(int i = 2; i<8;i++)
+  {
+    if(digitalRead(i) != prev_pinvalue[i])
+    {
+      if(prev_pinvalue[i]==0) //rising edge
+      {
+        prev_pinvalue[i] = 1;
+        pinmicros[i] = isrMicros;
+      }
+      else //falling edge
+      {
+        prev_pinvalue[i] = 0;
+        pinmicros[i] = isrMicros - pinmicros[i];
+      }
+    }
+  }
 }
 
