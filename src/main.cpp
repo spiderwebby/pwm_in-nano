00854ac49028f1c52dbd14e248859cf66e45a296
spiderwebby <spiderwebby@gmail.com>
2025-01-14 08:04:08 +0000

    comment block, shortened arrays

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
+// 
+// this pulls in 8x PWM signals and spits them out as serial CSV
+// it doesn't use pulseIn() because pulseIn sucks hard when you need to use it 8 times
+// PWM in on D2-D12
+// the arrays are too big, i don't care yet.
+// maybe i'll do some digitalRead(i+2) stuff at some point, but that point is not today  
+//
+
 #include <Arduino.h>
 
 
 const int maxReadingAge = 5000; //5ms
-volatile bool prev_pinvalue[15];
-volatile unsigned int pinmicros[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-//volatile unsigned int pin_high_time[15];
+volatile bool prev_pinvalue[13];
+volatile unsigned int pinmicros[13] = {0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 byte prev_PINB;
 byte prev_PIND;
 
-//! could probably merge the two for()'s by adding an "if(i<7) i=9;" or whatever
 
-//these are defined *elsewhere ooooOOOOooo*
 //we have to work out which pin changed, and if it's falling or rising
-ISR (PCINT0_vect) //port B isr //D12-D8 port B
+ISR (PCINT0_vect) //port B isr //D8-D12 port B
 {
   unsigned int isrMicros = micros();
   for(int i = 8; i<13;i++)
@@ -34,7 +39,7 @@ ISR (PCINT0_vect) //port B isr //D12-D8 port B
   }
 }
 
-ISR (PCINT2_vect)// D7-D2 port D
+ISR (PCINT2_vect)// D2-D7 port D
 {
   unsigned int isrMicros = micros();
   for(int i = 2; i<8;i++)
@@ -58,11 +63,7 @@ ISR (PCINT2_vect)// D7-D2 port D
 
 void setup() {
 
-  for(int i = 2; i<8;i++)  
-  {
-    pinMode(i,INPUT);
-  }
-  for(int i = 8; i<12;i++)
+  for(int i = 2; i<13;i++)  
   {
     pinMode(i,INPUT);
   }
@@ -71,7 +72,7 @@ void setup() {
   PCMSK0 |= B00011111; //D12-D8 port B
   PCMSK2 |= B11111100; // D7-D2 port D
 
-  sei();
+  sei(); //turn on le inturrupts
 
   Serial.begin(115200);
 
@@ -81,12 +82,7 @@ void printServos()
 {
     Serial.print("$srvo,");
   //we're not actually using all the pins, so we skip a bunch at the start and in the middle
-  for(int i = 2; i<8;i++)  
-  {
-    Serial.print(pinmicros[i]);
-    Serial.print(",");
-  }
-  for(int i = 8; i<12;i++)
+  for(int i = 2; i<12;i++)  
   {
     Serial.print(pinmicros[i]);
     Serial.print(",");
@@ -97,13 +93,7 @@ void printServos()
 void expireValues()
 {
   unsigned int currentMicros = micros();
-  for(int i = 2; i<8;i++)  
-  {
-    if(pinmicros[i]-maxReadingAge < currentMicros)
-      prev_pinvalue[i] = 0;
-  }
-
-  for(int i = 8; i<12;i++)
+  for(int i = 2; i<13;i++)  
   {
     if(pinmicros[i]-maxReadingAge < currentMicros)
       prev_pinvalue[i] = 0;
